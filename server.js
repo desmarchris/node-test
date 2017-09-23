6ec59a782d2e714d9f7e3b625350b9a034d19d8b
chris-desmarais <chris.desmarais@ibm.com>
2017-09-22 23:34:48 -0400

    added projects 2

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs');
+  res.render('projects.hbs', {
+    pageTitle: 'Project page'
+  });
 });
 
 // bad - send back json errorMessage
